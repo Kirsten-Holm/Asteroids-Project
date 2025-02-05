f3c1378cc5f7658be24c36fd2a9a2a5a0b8c4b03
Kirsten-Holm <dj4kbox@gmail.com>
2025-02-05 13:03:44 +0100

    Added player class and ability to turn left and right

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from constants import *
+from player import Player
+#from circleshape import 
 
 # this allows us to use code from
 # the open-source pygame library
@@ -11,11 +13,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill(BLACK_COLOUR)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         
